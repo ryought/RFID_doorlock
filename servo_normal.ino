48f4fd8ad4f432fdd130fdff705bf61de2f1a927
ryought <ryonakabayashi@gmail.com>
2011-12-22 19:31:20 +0900

    first commit

---
+++ servo_normal.ino
@@ -0,0 +1,15 @@
+#include <Servo.h>
+
+Servo servo;
+
+void setup(){
+  servo.attach(0);
+}
+
+void loop(){
+  for (int angle = 0 ; angle <= 179 ; angle++){
+    servo.write(angle);
+    delay(20);
+  }
+}
+
